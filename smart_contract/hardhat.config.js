d54c359272aec2c2269c1ac877e602c01e61e854
NebojsaStojakovic <nebojsastojakovicla@gmail.com>
2022-04-01 11:18:44 +0200

    fixed smart contract

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -4,8 +4,8 @@ module.exports = {
   solidity: '0.8.0',
   networks: {
     ropsten: {
-      url: 'https://eth-ropsten.alchemyapi.io/v2/z4WpA8UKgqnwbTYmrZu15yCOiijBKaRv',
-      accounts: ['2f99db8cdb04655028eee1dc98230925202f6b3e010e43fad2883b4bea90a1a3'],
+      url: 'https://eth-ropsten.alchemyapi.io/v2/gQMBO1Dv4-AlNrCtSlFCWCARkqQVoqmO',
+      accounts: ['e4c2f56d6ad62079aa7b6922f2a40de30af23e2fd86c33ec72e62b411914de9e'],
     },
   },
 };
